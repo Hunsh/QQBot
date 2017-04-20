2682aedcf83099bf3fbf62f64e5393e956afe459
Hunsh <327494410@qq.com>
2017-04-21 00:22:46 +0800

    shit

---
+++ qqbot/src/main/java/com/hunsh/qqbot/App.java
@@ -0,0 +1,13 @@
+package com.hunsh.qqbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
