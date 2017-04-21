8c97dad242c7f834faa71acfc6825b97fe9ed896
hunsh <hunsh@ilegendsoftdeMacBook-Pro-4.local>
2017-04-21 18:35:24 +0800

    shit

---
+++ qqbot-core/src/main/java/com/hunsh/qqbot/login/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.hunsh.qqbot.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : Edward Jia
+ * @Description :
+ * @Date : 17/4/21
+ * @Version :
+ */
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
--- qqbot-core/src/test/java/com/hunsh/qqbot/client/ClientTest.java
+++ qqbot-core/src/test/java/com/hunsh/qqbot/client/ClientTest.java
@@ -3,6 +3,12 @@ package com.hunsh.qqbot.client;
 import com.hunsh.qqbot.contant.Constants;
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -20,13 +26,21 @@ public class ClientTest {
         //t=0.09821339275864471
         //System.out.println( Math.pow(Math.random(), 18d));
 
-        URL url = new URL(Constants.FETCH_QR_URL);
+        URL url = new URL(Constants.FETCH_QR_URL+Math.pow(Math.random(), 18d));
 
-        URLConnection connection = url.openConnection();
+        URLConnection conn = url.openConnection();
+
+        conn.setRequestProperty("accept", "*/*");
+        conn.setRequestProperty("connection", "Keep-Alive");
+        conn.setRequestProperty("user-agent",
+                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+        conn.connect();
+
+
+        BufferedImage image = ImageIO.read(conn.getInputStream());
+
+        ImageIO.write(image, "jpg", new File(File.separator + "Users" + File.separator + "hunsh" + File.separator
+                + "myfiles" + File.separator + "test/qr.jpg"));
 
     }
-
-
-
-
 }
